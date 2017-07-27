1504f6ace73ca8af0f2c601b931cfc4b120e11b3
Tony Brock <abrock17@gmail.com>
2017-07-27 17:14:09 -0400

    hardened validation of function string; handling subtraction - needs refactoring

--- src/parse.py
+++ src/parse.py
@@ -1,9 +1,16 @@
-
+import re
 
 def parse(function_string):
-	if (function_string.startswith('+')):
-		def add(value):
-			return value + int(function_string[1:])
-		return add
+  arithmetic = re.compile('^[+-][0-9]+$')
+  if (arithmetic.match(function_string)):
+    term = int(function_string[1:])
+    if (function_string.startswith('+')):
+      def add(value):
+        return value + term
+      return add
+    else:
+      def subtract(value):
+        return value - term
+      return subtract
 
-	raise ValueError('Illegal function : %s' % function_string)
+  raise ValueError('Illegal function : %s' % function_string)
--- test/parse_test.py
+++ test/parse_test.py
@@ -9,12 +9,20 @@ class ParseTest(TestCase):
   def test_throws_an_error_when_an_unknown_function_is_passed(self):
     self.assertRaises(ValueError, parse.parse, 'nonsense')
 
+  def test_throws_an_error_when_a_partially_unknown_function_is_passed(self):
+    self.assertRaises(ValueError, parse.parse, '+nonsense')
+
   def test_returns_a_function_that_adds_a_number(self):
   	add_function = parse.parse('+1')
   	
   	self.assertEquals(1, add_function(0))
 
-  def test_returns_a_function_that_adds_a_different_number(self):
-  	add_function = parse.parse('+2')
+  def test_returns_a_function_that_adds_a_two_digit_number(self):
+  	add_function = parse.parse('+23')
   	
-  	self.assertEquals(4, add_function(2))  	
+  	self.assertEquals(25, add_function(2))
+
+  def test_returns_a_function_that_subtracts_a_number(self):
+  	add_function = parse.parse('-7')
+  	
+  	self.assertEquals(-7, add_function(0))
