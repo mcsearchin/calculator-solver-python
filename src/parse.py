a31265bbe644489dfc1b73d66b59f2d50391e249
Tony Brock <abrock17@gmail.com>
2017-07-27 17:00:48 -0400

    +x can be parsed to a funciton

--- src/parse.py
+++ src/parse.py
@@ -1,4 +1,9 @@
 
 
 def parse(function_string):
+	if (function_string.startswith('+')):
+		def add(value):
+			return value + int(function_string[1:])
+		return add
+
 	raise ValueError('Illegal function : %s' % function_string)
