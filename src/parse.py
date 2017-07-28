9951b169e95861846ee13874847c8a2f123ffdb0
Tony Brock <abrock17@gmail.com>
2017-07-28 11:59:53 -0400

    parsing the 'truncate' operation; some refactoring in parse

--- src/parse.py
+++ src/parse.py
@@ -1,17 +1,24 @@
 import re
 
 def parse_operation(operation_string):
-  arithmetic = re.compile('^[+\-*/][0-9]+$')
-  if (arithmetic.match(operation_string)):
-    operator = operation_string[0:1]
-    second_term = float(operation_string[1:])
 
-    return {
-      '+': lambda value: value + second_term,
-      '-': lambda value: value - second_term,
-      '*': lambda value: value * second_term,
-      '/': lambda value: value / second_term,
-    }[operator]
+  if _is_arithmetic_operator(operation_string):
+    return _generate_arithmetic_function(operation_string)
+  if '<<' == operation_string:
+    return lambda value: int(value) / 10
 
   raise ValueError('Illegal operation : %s' % operation_string)
 
+def _is_arithmetic_operator(operation_string):
+  return re.compile('^[+\-*/][0-9]+$').match(operation_string)
+
+def _generate_arithmetic_function(operation_string):
+  operator = operation_string[0:1]
+  second_term = float(operation_string[1:])
+
+  return {
+    '+': lambda value: value + second_term,
+    '-': lambda value: value - second_term,
+    '*': lambda value: value * second_term,
+    '/': lambda value: value / second_term,
+  }[operator]
--- test/parse_test.py
+++ test/parse_test.py
@@ -12,27 +12,37 @@ class ParseTest(TestCase):
   def test_raises_an_error_when_a_partially_unknown_function_is_passed(self):
     self.assertRaises(ValueError, self.subject.parse_operation, '+nonsense')
 
-  def test_returns_a_function_that_adds_a_number(self):
+  def test_parses_a_function_that_adds_a_number(self):
   	resulting_function = self.subject.parse_operation('+1')
   	
   	self.assertEquals(1, resulting_function(0))
 
-  def test_returns_a_function_that_adds_a_two_digit_number(self):
+  def test_parses_a_function_that_adds_a_two_digit_number(self):
   	resulting_function = self.subject.parse_operation('+23')
   	
   	self.assertEquals(25, resulting_function(2))
 
-  def test_returns_a_function_that_subtracts_a_number(self):
+  def test_parses_a_function_that_subtracts_a_number(self):
   	resulting_function = self.subject.parse_operation('-7')
   	
   	self.assertEquals(-7, resulting_function(0))
 
-  def test_returns_a_function_that_multiplies_a_number(self):
+  def test_parses_a_function_that_multiplies_a_number(self):
   	resulting_function = self.subject.parse_operation('*2')
   	
   	self.assertEquals(8, resulting_function(4))
 
-  def test_returns_a_function_that_divides_a_number(self):
+  def test_parses_a_function_that_divides_a_number(self):
   	resulting_function = self.subject.parse_operation('/4')
   	
   	self.assertEquals(-3, resulting_function(-12))
+
+  def test_parses_a_function_that_truncates_the_last_digit(self):
+    resulting_function = self.subject.parse_operation('<<')
+
+    self.assertEquals(78, resulting_function(789))
+
+  def test_parsed_truncating_function_also_removes_digits_after_the_decimal_point(self):
+    resulting_function = self.subject.parse_operation('<<')
+
+    self.assertEquals(7, resulting_function(78.9))
