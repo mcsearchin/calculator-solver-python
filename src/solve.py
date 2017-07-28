0b03a7f6995adea615f9f49c07aba858a3136b25
Tony Brock <abrock17@gmail.com>
2017-07-28 10:43:56 -0400

    now handling a three-step operation; pivoted to a recursive solution

--- src/solve.py
+++ src/solve.py
@@ -1,28 +1,26 @@
 import parse
 
-def find_steps_to_solve(initial_value, desired_value, number_of_operations, allowed_operations_set):
-  steps = _attempt_to_solve(initial_value, desired_value, number_of_operations, allowed_operations_set)
-  if steps:
-    return steps
+def find_steps_to_solve(initial_value, desired_value, number_of_steps, allowed_operations_set):
+  solution = _attempt_to_solve(initial_value, desired_value, number_of_steps, [], allowed_operations_set)
+  if solution:
+    return solution
+  else:
+    raise ValueError(
+      'Unable to calculate %s from %s in %s steps with the provided operations: %s' 
+      % (desired_value, initial_value, number_of_steps, allowed_operations_set)
+    )
 
-  steps = _attempt_to_solve(initial_value, desired_value, number_of_operations, allowed_operations_set[::-1])
-  if steps:
-    return steps
-    
-  raise ValueError(
-    'Unable to calculate %s from %s in %s steps with the provided operations: %s' 
-    % (desired_value, initial_value, number_of_operations, allowed_operations_set)
-  )
+def _attempt_to_solve(current_value, desired_value, number_remaining_steps, steps_so_far, allowed_operations_set):
+  if number_remaining_steps < 1:
+    return None
 
-def _attempt_to_solve(initial_value, desired_value, number_of_operations, allowed_operations_set):
-  working_value = initial_value
-  steps = []
-  for index in range(number_of_operations):
-    op_index = index if index < len(allowed_operations_set) else len(allowed_operations_set) - 1
-    operation_string = allowed_operations_set[op_index]
+  for operation_string in allowed_operations_set:
     operation = parse.parse_operation(operation_string)
-    working_value = operation(working_value)
-    steps.append(operation_string)
-    if working_value == desired_value:
+    new_value = operation(current_value)
+    steps = steps_so_far + [operation_string]
+    if new_value == desired_value:
       return steps
-
+    else:
+      possible_solution = _attempt_to_solve(new_value, desired_value, number_remaining_steps - 1, steps, allowed_operations_set)
+      if possible_solution:
+        return possible_solution
