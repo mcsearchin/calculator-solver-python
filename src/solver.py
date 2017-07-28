7ab0fbee6eb36730f6f8843c67653ce7ac3eeb05
Tony Brock <abrock17@gmail.com>
2017-07-28 07:34:30 -0400

    handling cases where the number of operations is too high or too low

--- src/solver.py
+++ src/solver.py
@@ -1,5 +1,15 @@
 
 
-def find_steps_to_solve(initial_value, desired_value, number_of_operations, operations):
-  return [operations[0] for index in range(number_of_operations)]
+def find_steps_to_solve(initial_value, desired_value, number_of_operations, allowed_operations):
+  working_value = initial_value
+  steps = []
+  for index in range(number_of_operations):
+    working_value += 1
+    steps.append(allowed_operations[0])
+    if working_value == desired_value:
+      return steps
 
+  raise ValueError(
+    'Unable to calculate %s from %s in %s steps with the provided operations: %s' 
+    % (desired_value, initial_value, number_of_operations, allowed_operations)
+  )
--- test/solver_test.py
+++ test/solver_test.py
@@ -6,13 +6,21 @@ class SolverTest(TestCase):
   def setUp(self):
     self.subject = solver
 
-  def test_solves_a_simple_single_operation_problem(self):
+  def test_finds_steps_to_solve_a_single_operation_problem(self):
     steps = self.subject.find_steps_to_solve(0, 1, 1, ['+1'])
 
     self.assertEquals(['+1'], steps)
 
-  def test_solves_a_simple_multi_step_single_operation_problem(self):
+  def test_finds_steps_to_solve_a_multi_step_single_operation_problem(self):
     steps = self.subject.find_steps_to_solve(0, 2, 2, ['+1'])
 
     self.assertEquals(['+1', '+1'], steps)
+
+  def test_finds_steps_to_solve_a_single_operation_problem_in_fewer_steps_than_provided(self):
+    steps = self.subject.find_steps_to_solve(0, 1, 2, ['+1'])
+
+    self.assertEquals(['+1'], steps)
+
+  def test_raises_an_error_when_not_enough_steps_are_provided_to_solve_the_problem(self):
+    self.assertRaises(ValueError, self.subject.find_steps_to_solve, 0, 3, 2, ['+1'])
 
